07b5c8e55b27e843eb68af866835527fe41dac31
ddong8292 <ddong8292@gmail.com>
2016-08-10 17:46:40 +0900

    8-10

---
+++ GUI0810/src/com/sds/collection/MemberModel.java
@@ -0,0 +1,47 @@
+/*
+	JTable은 껍데기에 불과하기 때문에 실제 데이터를 연동한 객체인 TableModel을 정의하여, 회원관리 프로그램에 출력할
+	JTable에 보여질 데이터를 처리해보자!
+ * */
+package com.sds.collection;
+
+import java.util.ArrayList;
+
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableModel;
+
+public class MemberModel extends AbstractTableModel{
+	String[] column={"ID","Password","Name"};
+	//String[][] data=new String[0][0];
+	
+	//배열은 그 크기가 고정되어 있으므로, 컬렉션 프레임웍을 이용하여 기능을 개선하자!
+	ArrayList<String[]> list=new ArrayList<String[]>();
+	
+	
+	public int getRowCount() {
+		return list.size();
+	}
+	public int getColumnCount() {
+		return column.length;
+	}
+	public Object getValueAt(int row, int col) {
+		String[]record=list.get(row);
+		return record[col];
+	}
+	public String getColumnName(int col) {
+		return column[col];
+	}
+	public boolean isCellEditable(int row, int col) {
+	
+		if(col==1){
+			return false;
+			
+		}else{
+			return true;
+		}
+	}
+	public void setValueAt(Object v, int row, int col) {
+		System.out.println(v+"값을"+row+","+col+"에 넣을래");
+		String[] record=list.get(row);
+		record[col]=(String)v;
+	}
+}
---
+++ GUI0810/src/com/sds/file/Test.java
@@ -0,0 +1,124 @@
+package com.sds.file;
+
+import java.awt.BorderLayout;
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Test extends JFrame implements ActionListener{
+	JPanel pan;
+	JPanel pan2;
+	JLabel la;
+	JLabel la2;
+	JTextField txt;
+	JTextField txt2;
+	JButton bt1;
+	JButton bt2;
+	JButton bt3;
+	JFileChooser chooser;
+	JFileChooser chooser2;
+	FileInputStream fis;
+	FileOutputStream fos;
+	
+	public Test(){
+		pan=new JPanel();
+		pan2=new JPanel();
+		la=new JLabel("원본경로");
+		la2=new JLabel("저장경로");
+		txt=new JTextField(15);
+		txt2=new JTextField(15);
+		bt1=new JButton("열기");
+		bt2=new JButton("Save");
+		bt3=new JButton("실행");
+		
+		pan.setLayout(new BorderLayout());
+		pan.add(la, BorderLayout.WEST);
+		pan.add(txt, BorderLayout.CENTER);
+		pan.add(bt1, BorderLayout.EAST);
+		
+		pan2.setLayout(new BorderLayout());
+		pan2.add(la2, BorderLayout.WEST);
+		pan2.add(txt2, BorderLayout.CENTER);
+		pan2.add(bt2, BorderLayout.EAST);
+		
+		add(pan, BorderLayout.NORTH);	
+		add(pan2, BorderLayout.CENTER);
+		add(bt3, BorderLayout.SOUTH);
+		
+		pan.setSize(500,80);
+		pan2.setSize(500,80);
+		
+		bt1.addActionListener(this);
+		bt2.addActionListener(this);
+		bt3.addActionListener(this);
+		
+		chooser=new JFileChooser();
+		chooser2=new JFileChooser();
+		
+		setSize(500,200);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+	public void Open(){
+		int result=chooser.showOpenDialog(this);
+		System.out.println("당신의 선택은?"+result);
+		if(result==JFileChooser.APPROVE_OPTION){
+			//지금 선택한 이미지를 프로필 사진에 반영
+			File file=chooser.getSelectedFile();
+			String path=file.getAbsolutePath();
+			System.out.println(path);
+			txt.setText(path);
+			repaint();
+		}
+	}
+	public void Save(){
+		int result=chooser2.showOpenDialog(this);
+		if(result==JFileChooser.APPROVE_OPTION){
+			String path=chooser2.getSelectedFile().getParent();
+			System.out.println(path);
+			txt2.setText(path);
+			repaint();
+		}
+	}
+	public void Go(){
+		try{
+			fis = new FileInputStream(txt.getText());
+			fos = new FileOutputStream(txt2.getText());
+			
+		}catch(FileNotFoundException e){
+			
+		}finally{
+			
+		}
+	
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		if(bt1==e.getSource()){
+			Open();
+		}else if(bt2==e.getSource()){
+			Save();
+		}else if(bt3==e.getSource()){
+			Go();
+		}
+	}
+	public static void main(String[] args) {
+		new Test();
+	}
+}
